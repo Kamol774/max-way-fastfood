011ed5761a1004c437ae89c483a8611d499a6572
KAMOLIDDIN <chust1222@gmail.com>
2021-08-12 17:42:09 +0500

    'base_changed'

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -78,18 +78,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.getenv('DB_NAME', 'maxway_db'),
+#         'USER':os.getenv('DB_USER', 'maxway_user'),
+#         'PASSWORD':os.getenv('DB_PASSWORD', 'root'),
+#         'HOST':os.getenv('DB_HOST', '127.0.0.1'),
+#         'PORT':os.getenv('DB_PORT', '5432'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DB_NAME', 'maxway_db'),
-        'USER':os.getenv('DB_USER', 'maxway_user'),
-        'PASSWORD':os.getenv('DB_PASSWORD', 'root'),
-        'HOST':os.getenv('DB_HOST', '127.0.0.1'),
-        'PORT':os.getenv('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
